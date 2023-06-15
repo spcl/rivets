c6f927c592d95a428bb3f64e6e303a402bf02b03
Andrei Ivanov <9387186+and-ivanov@users.noreply.github.com>
2023-06-15 16:57:28 +0200

    Change layernorm implementaiton (too difficult for llvm toolchain so it doesn't work)

--- dnn_kernels/src/layernorm.c
+++ dnn_kernels/src/layernorm.c
@@ -451,12 +451,12 @@ void layer_norm_raw_fp64_sdma_ssr_frep(
 
     if (tid == 0) {
         snrt_ssr_loop_3d(SNRT_SSR_DM0,
-            N, 4, batch_buf_size, 
+            N, 3, batch_buf_size,
             sizeof(double), 0, N * sizeof(double)
         );
-        snrt_ssr_loop_4d(SNRT_SSR_DM1,
-            unroll, 2, N / unroll, batch_buf_size,
-            2 * sizeof(double), sizeof(double), 2 * sizeof(double) * unroll, 0
+        snrt_ssr_loop_3d(SNRT_SSR_DM1,
+            N, 2, batch_buf_size,
+            2 * sizeof(double), sizeof(double), 0
         );
         snrt_ssr_loop_3d(SNRT_SSR_DM2,
             N, 2, batch_buf_size,
@@ -499,18 +499,16 @@ void layer_norm_raw_fp64_sdma_ssr_frep(
 
         if (tid == 0) {
             snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_3D, tmp_buf0);
-            snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_4D, gam_buf);
+            snrt_ssr_read(SNRT_SSR_DM1, SNRT_SSR_3D, gam_buf);
             snrt_ssr_write(SNRT_SSR_DM2, SNRT_SSR_3D, tmp_buf0);
 
             snrt_ssr_enable();
             for (size_t b2 = 0; b2 < b2_len; b2 += 1) {
                 size_t b = b1 + b2;
 
-                double r[unroll];
+                double r0, r1, r2, r3;
 
-                for (size_t i = 0; i < unroll; i++) {
-                    r[i] = 0;
-                }
+                r0 = r1 = r2 = r3 = 0;
 
                 asm volatile(
                     "frep.o %[reps], %[unroll], 0, 0;"
@@ -518,7 +516,7 @@ void layer_norm_raw_fp64_sdma_ssr_frep(
                     "fadd.d %[mu1], ft0, %[mu1];"
                     "fadd.d %[mu2], ft0, %[mu2];"
                     "fadd.d %[mu3], ft0, %[mu3];"
-                    : [mu0] "+f"(r[0]), [mu1] "+f"(r[1]), [mu2] "+f"(r[2]), [mu3] "+f"(r[3])
+                    : [mu0] "+f"(r0), [mu1] "+f"(r1), [mu2] "+f"(r2), [mu3] "+f"(r3)
                     : [reps] "r"(N / unroll - 1), [unroll] "i"(unroll)
                     : "ft0", "ft1", "ft2", "memory"
                 );
@@ -527,36 +525,37 @@ void layer_norm_raw_fp64_sdma_ssr_frep(
                     "fadd.d %0, %0, %2;"
                     "fadd.d %1, %1, %3;"
                     "fadd.d %0, %0, %1;"
-                    : "+f"(r[0]), "+f"(r[2])
-                    : "f"(r[1]), "f"(r[3])
+                    : "+f"(r0), "+f"(r2)
+                    : "f"(r1), "f"(r3)
                     : "ft0", "ft1", "ft2", "memory"
                 );
 
-                double mu = r[0];
+                double mu = r0;
 
                 asm volatile("fmul.d %0, %0, %1" : "+f"(mu) : "f"(1. / N) : "ft0", "ft1", "ft2", "memory");
 
-                asm volatile(
-                    "frep.o %[rep], 1, 0, 0;"
-                    "fsub.d ft2, ft0, %[mu];"
-                    :
-                    : [mu] "f"(mu)
-                    , [rep] "r"(N - 1)
-                    : "ft0", "ft1", "ft2", "memory"
-                );
+                r0 = r1 = r2 = r3 = 0;
 
-                for (size_t i = 0; i < unroll; i++) {
-                    r[i] = 0;
-                }
+                double t0, t1, t2, t3;
 
                 asm volatile(
                     "frep.o %[rep], %[unroll], 0, 0;"
-                    "fmadd.d %[s0], ft0, ft0, %[s0];"
-                    "fmadd.d %[s1], ft0, ft0, %[s1];"
-                    "fmadd.d %[s2], ft0, ft0, %[s2];"
-                    "fmadd.d %[s3], ft0, ft0, %[s3];"
-                    : [s0] "+f"(r[0]), [s1] "+f"(r[1]), [s2] "+f"(r[2]), [s3] "+f"(r[3])
-                    : [rep] "r"(N / unroll - 1), [unroll] "i"(unroll)
+                    "fsub.d ft4, ft0, %[mu];"
+                    "fmadd.d %[s0], ft4, ft4, %[s0];"
+                    "fmul.d ft2, ft4, ft1;"
+                    "fsub.d ft4, ft0, %[mu];"
+                    "fmadd.d %[s1], ft4, ft5, %[s1];"
+                    "fmul.d ft2, ft4, ft1;"
+                    "fsub.d ft4, ft0, %[mu];"
+                    "fmadd.d %[s2], ft4, ft6, %[s2];"
+                    "fmul.d ft2, ft4, ft1;"
+                    "fsub.d ft4, ft0, %[mu];"
+                    "fmadd.d %[s3], ft4, ft7, %[s3];"
+                    "fmul.d ft2, ft4, ft1;"
+                    : [s0] "+f"(r0), [s1] "+f"(r1), [s2] "+f"(r2), [s3] "+f"(r3)
+                    , [t0] "+f"(t0), [t1] "+f"(t1), [t2] "+f"(t2), [t3] "+f"(t3)
+                    : [mu] "f"(mu)
+                    , [rep] "r"(N / unroll - 1), [unroll] "r"(3 * unroll)
                     : "ft0", "ft1", "ft2", "memory"
                 );
 
@@ -564,12 +563,12 @@ void layer_norm_raw_fp64_sdma_ssr_frep(
                     "fadd.d %0, %0, %2;"
                     "fadd.d %1, %1, %3;"
                     "fadd.d %0, %0, %1;"
-                    : "+f"(r[0]), "+f"(r[2])
-                    : "f"(r[1]), "f"(r[3])
+                    : "+f"(r0), "+f"(r2)
+                    : "f"(r1), "f"(r3)
                     : "ft0", "ft1", "ft2", "memory"
                 );
 
-                double std = r[0];
+                double std = r0;
 
                 asm volatile(
                     "fmadd.d %[s], %[s], %[x], %[eps];"
@@ -582,20 +581,18 @@ void layer_norm_raw_fp64_sdma_ssr_frep(
 
                 asm volatile(
                     "frep.o %[rep], %[unroll], 0, 0;"
-                    "fmul.d %[t0], ft1, %[s];"
-                    "fmul.d %[t1], ft1, %[s];"
-                    "fmul.d %[t2], ft1, %[s];"
-                    "fmul.d %[t3], ft1, %[s];"
-                    "fmadd.d ft2, %[t0], ft0, ft1;"
-                    "fmadd.d ft2, %[t1], ft0, ft1;"
-                    "fmadd.d ft2, %[t2], ft0, ft1;"
-                    "fmadd.d ft2, %[t3], ft0, ft1;"
-                    : [t0] "=&f"(r[0]), [t1] "=&f"(r[1]), [t2] "=&f"(r[2]), [t3] "=&f"(r[3])
+                    "fmadd.d ft2, %[s], ft0, ft1;"
+                    "fmadd.d ft2, %[s], ft0, ft1;"
+                    "fmadd.d ft2, %[s], ft0, ft1;"
+                    "fmadd.d ft2, %[s], ft0, ft1;"
+                    :
                     : [s] "f"(std)
-                    , [rep] "r"(N / unroll - 1), [unroll] "i"(2 * unroll)
+                    , [rep] "r"(N / unroll - 1), [unroll] "i"(unroll)
                     : "ft0", "ft1", "ft2", "memory"
                 );
             }
+            __builtin_ssr_barrier(SNRT_SSR_DM0);
+            __builtin_ssr_barrier(SNRT_SSR_DM1);
             __builtin_ssr_barrier(SNRT_SSR_DM2);
             snrt_ssr_disable();
         }
